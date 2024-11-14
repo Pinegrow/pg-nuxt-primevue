dcda6346b1f5ac04c68b65ad018740df6897eba5
Akayy <techakayy@gmail.com>
2024-11-14 12:38:39 +1100

    Minimal template

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,21 +1,11 @@
-<script setup lang="ts">
-  //...
-</script>
-
 <template>
   <div>
     <main>
-      <DarkModeSwitch
-        style="
-          position: absolute;
-          right: 0;
-          z-index: 10;
-          margin-top: 12px;
-          margin-bottom: 12px;
-        "
-      />
       <slot />
     </main>
   </div>
 </template>
-<style></style>
+<style>
+  @import '~/assets/css/global.css';
+  @import '~/assets/css/main.css';
+</style>
