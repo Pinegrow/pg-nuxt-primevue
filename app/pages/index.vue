4060ffea8ebc3dee2e137d3e3f922283f179e8b8
Akayy <techakayy@gmail.com>
2024-11-14 11:33:08 +1100

    Initial commit

---
+++ app/pages/index.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+  import { heroImageUrl } from '@/utils/hero'
+
+  definePageMeta({
+    // layout: 'default',
+    // name: 'index',
+    // alias: 'index',
+    title: 'Home',
+    description:
+      "Unlock the Web's Potential with Nuxt: Building Tomorrow's Today!",
+    navOrder: 1,
+    type: 'primary',
+    icon: 'i-mdi-home',
+    // ogImage: 'images/ogImage.png', // url or local images inside public folder, for eg, ~/public/images/ogImage.png
+  })
+
+  const { optimizeImage } = useOptimizeImage()
+  const heroImageOptimized = {
+    alt: `hero`,
+    cover: true,
+    ...optimizeImage(
+      heroImageUrl,
+      /* options */
+      {
+        // placeholder: false, // placeholder image before the actual image is fully loaded.
+      },
+      true /* return bgStyles */,
+    ),
+  }
+
+  const heroImage = heroImageOptimized.src
+  const bgStyles = heroImageOptimized.bgStyles
+</script>
+<template>
+  <TheWelcome />
+</template>
+<style scoped></style>
