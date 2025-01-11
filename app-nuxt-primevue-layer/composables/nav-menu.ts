04f8b90b26f4a75afb414df9a2f67d550e5065f8
Akayy <techakayy@gmail.com>
2025-01-11 23:04:42 +1100

    Meta updated

--- app-nuxt-primevue-layer/composables/nav-menu.ts
+++ app-nuxt-primevue-layer/composables/nav-menu.ts
@@ -3,24 +3,19 @@ import siteMeta from '@/site'
 export const useNavMenu = () => {
   const navs = siteMeta.navs
 
+  const allNavs = Object.values(navs).reduce((acc, navMenu) => {
+    return [...acc, ...navMenu]
+  }, [])
+
   const currentRoute = useRoute()
   const currentPath = computed(() => {
     return currentRoute.path
   })
 
   return {
-    navs,
+    allNavs,
     navsPrimary: navs.primary,
     navsSecondary: navs.secondary,
     currentPath,
   }
-}
-
-export const isCurrentRoute = (navlink, currentPath) => {
-  if (!currentPath) {
-    currentPath = useNavMenu().currentPath.value
-  }
-  return navlink.link === '/'
-    ? currentPath === navlink.link
-    : currentPath.startsWith(navlink.link)
 }
